43cb6b7614cd946278009de4c77566adfbe6e3e1
Starlin de Jesus Medina <starlinjesus612@gmail.com>
2022-04-13 15:50:17 -0400

    create Btn_icon component

---
+++ components/Btn_icon.js
@@ -0,0 +1,14 @@
+import React from "react";
+import { TouchableOpacity } from "react-native";
+
+import { Colors } from "../styles/styled";
+
+const Btn_icon = ({onPress,icon, styleButton}) => {
+  return (
+    <TouchableOpacity onPress={onPress} style={styleButton}>
+      {icon}
+    </TouchableOpacity>
+  );
+};
+
+export default Btn_icon;
