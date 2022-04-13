67866731e00f92d15d98e70998ed0bbed9d7c440
Starlin de Jesus Medina <starlinjesus612@gmail.com>
2022-04-13 16:24:55 -0400

    update file Contact.js in screens

--- screens/Contact.js
+++ screens/Contact.js
@@ -3,8 +3,6 @@ import { View, FlatList, Text, Image, Modal } from "react-native";
 
 //Iconos
 import { MaterialIcons } from "@expo/vector-icons";
-import { FontAwesome5 } from "@expo/vector-icons";
-import { AntDesign } from "@expo/vector-icons";
 import { Entypo } from "@expo/vector-icons";
 //Imagenes
 import contanctUs from "../assets/contanctUs.png";
@@ -21,6 +19,10 @@ import { Colors } from "../styles/styled";
 import contact from "../styles/contact_page";
 import Btn_icon from "../components/Btn_icon";
 import btnDrawerStyle from "../styles/btnDrawerStyle";
+
+import { Links } from "../helpers/Links";
+import { Call } from "../helpers/Call";
+
 
 const Contact = ({ navigation }) => {
   const [isCallModalOpen, openCallModal, closeCallModal] = useModal(false);
@@ -126,50 +128,6 @@ const Contact = ({ navigation }) => {
   );
 };
 
-const Links = [
-  {
-    idx: 1,
-    icon: (
-      <MaterialIcons name="phone-iphone" size={24} color={Colors.secondary} />
-    ),
-    title: "Centro de contacto",
-    sudtitle: "(809)-243-0000",
-  },
-  {
-    idx: 2,
-    icon: <FontAwesome5 name="globe" size={24} color={Colors.secondary} />,
-    title: "Llamas desde el exterior",
-    sudtitle: "1-000-000-0000",
-  },
-  {
-    idx: 3,
-    icon: <MaterialIcons name="email" size={24} color={Colors.secondary} />,
-    title: "Correo electronico",
-    sudtitle: "cliente@gmail.com",
-  },
-];
 
-const Call = [
-  {
-    idx: 4,
-    icon: (
-      <AntDesign name="facebook-square" size={24} color={Colors.secondary} />
-    ),
-    title: "Facebook",
-    sudtitle: "/banco_RD",
-  },
-  {
-    idx: 5,
-    icon: <AntDesign name="twitter" size={24} color={Colors.secondary} />,
-    title: "Twitter",
-    sudtitle: "@banco_RD",
-  },
-  {
-    idx: 6,
-    icon: <AntDesign name="instagram" size={24} color={Colors.secondary} />,
-    title: "Instagram",
-    sudtitle: "@Banco_RD2",
-  },
-];
 
 export default Contact;
